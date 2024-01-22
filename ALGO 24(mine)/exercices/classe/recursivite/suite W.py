85b98e4990adc3cd429a04e067a3ebd39592f6a2
azisdiri <azizsdiri40@gmail.com>
2024-01-22 22:36:13 +0100

    #

--- ALGO 24(mine)/exercices/classe/recursivite/suite W.py
+++ ALGO 24(mine)/exercices/classe/recursivite/suite W.py
@@ -1,19 +1,7 @@
-'''def W():
-    p =  1
-    for  i in range(1,n+1):
-        p=p+2*(i+1)
-    return p
-n = int(input('donner n : '))
-print(W())    
-    '''
-
-
 def W(n):
     if n == 0:
         return 1
     else:
-        return W(n-1) + 2*(n+1)
-
-
+        return W(n-1) + 2*(n)
 n = int(input('donner n : '))
 print(W(n))
