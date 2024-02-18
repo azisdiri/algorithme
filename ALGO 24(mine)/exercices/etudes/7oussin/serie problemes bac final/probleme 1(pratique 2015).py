b295c4bd3fdf6dbf220f543a5adc43276d04b129
azisdiri <109858507+azisdiri@users.noreply.github.com>
2024-02-18 11:51:30 +0100

    Update probleme 1(pratique 2015).py

--- ALGO 24(mine)/exercices/etudes/7oussin/serie problemes bac final/probleme 1(pratique 2015).py
+++ ALGO 24(mine)/exercices/etudes/7oussin/serie problemes bac final/probleme 1(pratique 2015).py
@@ -1,19 +1,3 @@
-from pickle import *
-from numpy import array
-def dist(t,n,x):
-    j = 0
-    while (j<=n-1) and (t[j]!=x):
-        j += 1
-    return j == n
-
-
-def nbtermes(x,j):
-    if x == 1:
-        return j + 1
-    elif x % 2 == 0:
-        return nbtermes(x // 2, j + 1)
-    else:
-        return nbtermes(x * 3 + 1, j + 1)
 
 def saisir():
     global p
@@ -21,7 +5,11 @@ def saisir():
     while not (p<=30 and p>=2):
         p = int(input('donner n : '))
         
-        
+def dist(t,n,x):
+    j = 0
+    while (j<=n-1) and (t[j]!=x):
+        j += 1
+    return j == n    
 def remplir():
     global f1
     f1 = open('Depart.dat','wb')
@@ -38,33 +26,58 @@ def remplir():
         t[i] = u0
         dump(u0,f1)
     f1.close()
-    
+def nbtermes(x,j):
+    if x == 1:
+        return j
+    else:
+        if x%2 == 0:
+            return nbtermes(x//2,j+1)
+        else:
+            return nbtermes(x*3+1,j+1)
 def generer():
     global f2
-    f1 = open("Depart.dat","rb")
-    f2 = open("Suite.dat","wb")
+    f1=open("Depart.dat","rb")
+    f2=open("Suite.dat","wb")
     for i in range(p):
-        u0 = load(f1)
-        e = {}
-        e["dep"] = u0
-        e["nb"] = nbtermes(u0,0)
+        u0=load(f1)
+        e={}
+        e["dep"]=u0
+        e["nb"]=nbtermes(u0,0)
         dump(e,f2)
     f1.close()
     f2.close()
-    
-def afficher():
-    f2 = open("Suite.dat","rb")
-    fin_fichier = False
-    while not (fin_fichier):
+def rech_min(f2):
+    f2=open("Suite.dat","rb")
+    e=load(f2)
+    min=e["nb"]
+    eof=False
+    while not(eof):
         try:
-            e = load(f2)
-            print("x = ",e["dep"],"nobre de termes",e["nb"])
+            e=load(f2)
+            if e["nb"]<min:
+                min=e["nb"]
         except:
-            fin_fichier == False
+            eof=True
+    f2.close()
+    return min
 
+def afficher(f2):
+    minimum=rech_min(f2)    
+    f2=open("Suite.dat","rb")
+    eof=False
+    print("les nombres des termes de depart qui correspondent au nombre de termes minimal",minimum,"sont:")
+    while not (eof):
+        try:
+            e=load(f2)
+            if (e["nb"]==minimum):
+                print(e["dep"])
+        except:
+            eof=True
 
+from pickle import dump,load
+from numpy import array
 ##pp
 saisir()
 remplir()
 generer()
-afficher()
+afficher(f2)
