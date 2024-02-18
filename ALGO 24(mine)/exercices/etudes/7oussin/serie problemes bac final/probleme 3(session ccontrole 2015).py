05ccd292492538499c4978a6c693931780b6e9e0
azisdiri <109858507+azisdiri@users.noreply.github.com>
2024-02-18 12:32:47 +0100

    Update probleme 3(session ccontrole 2015).py

--- ALGO 24(mine)/exercices/etudes/7oussin/serie problemes bac final/probleme 3(session ccontrole 2015).py
+++ ALGO 24(mine)/exercices/etudes/7oussin/serie problemes bac final/probleme 3(session ccontrole 2015).py
@@ -4,26 +4,26 @@ from numpy import array
 
 def saisir():
     global n
-    n = int(input("taille : "))
+    n=int(input("taille : "))
     while not (n <= 200 and n >= 3):
-        n = int(input("taille : "))
+        n=int(input("taille : "))
 
 
 def remplir():
     global f1
-    f1 = open("Processus.dat", "rb")
+    f1=open("Processus.dat", "rb")
     for i in range(n):
-        e = {}
-        e["code"] = "P"+str(i+1)
-        e["duree"] = int(input("duree:"))
+        e={}
+        e["code"]="P"+str(i+1)
+        e["duree"]=int(input("duree:"))
         while not (e["duree"] >= 0):
-            e["duree"] = int(input("duree:"))
-        dump(e.f1)
+            e["duree"]=int(input("duree:"))
+        dump(e["f1"])
     f1.close()
 
 
-def sjf():
-    t = array([{}]*200)
+def classer():
+    t=array([{}]*200)
     transfert_1()
     tri()
     transfert_2()
@@ -31,32 +31,29 @@ def sjf():
 
 def transfert_1():
     global t
-    f1 = open("Processus.dat", "rb")
+    f1=open("Processus.dat", "rb")
     for i in range(n):
-        e = {}
+        e={}
         load(f, e)
-        t[i]["code"] = e["code"]
-        t[i]["duree"] = e["duree"]
+        t[i]["code"]=e["code"]
+        t[i]["duree"]=e["duree"]
     f1.close
 
 
 def tri():
     global t
     for i in range(1, i):
-        aux = t[i]["duree"]
-        aux1 = t[i]["code"]
-        j = i
-        while (aux < t[j-1]["duree"]) and (j > 0):
-            t[j]["duree"] = t[j-1]["duree"]
-            t[j]["code"] = t[j-1]["code"]
+        aux=t[i]
+        j=i
+        while (aux["duree"]< t[j-1]["duree"]) and (j > 0):
+            t[j]=t[j-1]
             j -= 1
-        t[j]["duree"] = aux
-        t[j]["code"] = aux1
+        t[j]=aux
 
 
 def transfert_2():
     global f2
-    f2 = open("Ord_SJF.dat", "wb")
+    f2=open("Ord_classer.dat", "wb")
     for i in range(n):
         dump(t[i], f2)
     f2.close()
@@ -64,23 +61,29 @@ def transfert_2():
 
 def generer():
     global f3
-    f1 = open("Processus.dat", "rb")
-    f2 = open("Ord_SJF.dat", "rb")
-    f3 = open("Ord_Nouv.txt", "w")
-    ff1 = False
-    ff2 = False
-    e1 = load(f1)
-    e2 = load(f2)
-    while (not (ff1) and not(ff2) and (e1 = e2)):
+    f1=open("Processus.dat", "rb")
+    f2=open("Ord_classer.dat", "rb")
+    f3=open("Ord_Nouv.txt", "w")
+    eof1=False
+    eof2=False
+    while (not (eof1) and not(eof2)):
+        e1=load(f1)
+        e2=load(f2)
+        if()
         f3.write(e1["code"])
     f2.close()
     f1.cose()
-    f2 = open("Ord_SJF.dat","rb")
-    while 
-
+    f2=open("Ord_classer.dat","rb")
+    eof2=False
+    ch=load(f3)
+    e=load(f2)
+    while not(eof2 and ch!=e["nb"]):
+        write(f3,ch+"\n")
+    f2.close()
+    f3.close()
 
 # pp
 saisir()
 remplir()
-sjf()
+classer()
 generer()
